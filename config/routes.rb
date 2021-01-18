f4906af57cae34a135277fe1328443eb314de881
Nakajima3106 <uver777.333@gmail.com>
2021-01-18 06:35:25 +0000

    フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
   patch 'users/withdraw' => 'users#withdraw', as: 'withdraw'
   put 'users/withdraw' => 'users#withdraw'
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
